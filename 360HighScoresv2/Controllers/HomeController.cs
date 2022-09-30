248cc91d4b670da4144e86fcfe3c2d2f21c41502
pandekager8 <johannes@vad-mortensen.dk>
2022-09-30 13:03:32 +0200

    Fil upload til computer

--- 360HighScoresv2/Controllers/HomeController.cs
+++ 360HighScoresv2/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace _360HighScoresv2.Controllers
 {
@@ -24,6 +25,33 @@ namespace _360HighScoresv2.Controllers
         public ActionResult Indsendscore()
         {
             return View();
+        }
+        
+        [HttpPost]
+        
+        public ActionResult Indsendscore(HttpPostedFileBase file)
+        
+        
+        {
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+                    string filename = Path.GetFileName(file.FileName);
+                    string filepath = Path.Combine(Server.MapPath("~/Billeder"), filename);
+                    file.SaveAs(filepath);
+                }
+                ViewBag.Message = "Billedet er indsendt korrekt!";
+                return View();
+            }
+
+            catch
+            {
+                ViewBag.Message = "Billedet er ikke indsendt korrekt!";
+                return View();
+            }
+            
+            
         }
 
     }
